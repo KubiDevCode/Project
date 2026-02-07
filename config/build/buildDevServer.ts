6ddf5a07a14894ebf9793b4650c5899491dd7093
Kubi <stepancrazy89@gmail.com>
2026-02-07 23:18:12 +0300

    build: add development server configuration

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,9 @@
+import { Configuration as DevServerConfigurations } from "webpack-dev-server";
+import { BuildOptions } from "./types/config";
+
+export function buildDevServer(options: BuildOptions): DevServerConfigurations {
+    return {
+        port: options.port,
+        open: true,
+    }
+}
