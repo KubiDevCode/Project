fc3ece9bed880e91cfe8b323108bba4477a29a3c
Kubi <stepancrazy89@gmail.com>
2026-02-09 16:42:14 +0300

    refactor: adopt feature sliced structure

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,8 +1,9 @@
-import { Theme } from "./ThemeContext";
-import { ThemeContext } from "./ThemeContext";
+import { LOCAL_STORAGE_KEY } from "../lib/ThemeContext";
+import { Theme } from "../lib/ThemeContext";
+import { ThemeContext } from "../lib/ThemeContext";
 import { FC, useMemo, useState } from "react";
 
-const defaultTheme = localStorage.getItem('theme') as Theme || Theme.LIGHT
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_KEY) as Theme || Theme.LIGHT
 
 export const ThemeProvider: FC = ({ children }) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme)
