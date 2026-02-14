b2c8dd0af1a54ebde20cc86114fddac4008d63a7
Kubi <stepancrazy89@gmail.com>
2026-02-15 00:11:14 +0300

    feat: add error boundary handling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Suspense } from 'react';
-import { AppRouter } from './router';
+import { AppRouter } from './providers/router';
 import { Navbar } from '../widgets/Navbar';
 import { Sidebar } from '../widgets/Sidebar/inedx';
 
