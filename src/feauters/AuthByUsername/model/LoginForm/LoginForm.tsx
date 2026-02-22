87e4ed394d3d18f605fe6d47f38763a5e847b3b8
Kubi <stepancrazy89@gmail.com>
2026-02-22 21:34:35 +0300

    test: add auth form visual cases

--- src/feauters/AuthByUsername/model/LoginForm/LoginForm.tsx
+++ src/feauters/AuthByUsername/model/LoginForm/LoginForm.tsx
@@ -1,9 +1,9 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import s from './LoginForm.module.scss';
 import { Button } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input';
 import { useRef } from 'react';
+import s from './LoginForm.module.scss';
 
 interface LoginFormProps {
     className?: string;
@@ -14,8 +14,8 @@ export const LoginForm = ({ className }: LoginFormProps) => {
 
     return (
         <div className={classNames(s.LoginForm, {}, [className])}>
-            <Input className={s.input} type="text" autofocus placeholder='Введите username' />
-            <Input className={s.input} type="text" placeholder='Введите username' />
+            <Input className={s.input} type="text" autofocus placeholder={t('LoginForm')} />
+            <Input className={s.input} type="text" placeholder={t('PassForm')} />
             <Button
                 className={s.loginBtn}
             >
