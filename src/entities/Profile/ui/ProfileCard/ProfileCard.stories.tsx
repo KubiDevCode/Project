2c4049ec904443b73561a99bf5ae412fd4023e20
Kubi <stepancrazy89@gmail.com>
2026-07-27 14:17:25 +0300

    chore: update app branding and mock data

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -21,7 +21,7 @@ Primary.args = {
         username: 'admin',
         age: 22,
         country: Country.Ukraine,
-        lastname: 'ulbi tv',
+        lastname: 'ivan petrov',
         first: 'asd',
         city: 'asf',
         currency: Currency.USD,
